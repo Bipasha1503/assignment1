164542350442ee02cf5940fb577582556577140a
Bipasha1503 <116502091+Bipasha1503@users.noreply.github.com>
2022-10-25 13:31:26 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,24 @@
+function show1(name){
+    var text="I am Breakfast"
+    alert (text)
+}
+function show2(name){
+    var text="I am Lunch"
+    alert (text)
+}
+function show3(name){
+    var text="I am Snacks"
+    alert (text)
+}
+function show4(name){
+    var text="I am Dinner"
+    alert (text)
+}
+function show5(name){
+    var text="I am Drinks"
+    alert (text)
+}
+function show6(name){
+    var text="I am Party Night"
+    alert (text)
+}
